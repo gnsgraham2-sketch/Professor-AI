70b28ecda53565181cfedb1dd9ed6a01c642e50c
gnsgraham2-sketch <gnsgraham2@gmail.com>
2026-09-18 15:07:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -158,7 +158,7 @@ ad_code = """
 # Set the height and width slightly larger than the ad to avoid scrollbars
 components.html(ad_code, width=180, height=320)
 st.markdown("""
-    <style>
+<style>
     .stApp { background-color: #7FFFD4; color: #111E25; }
     h1, h2, h3 { color: #111E25 !important; font-family: 'Helvetica Neue', sans-serif; }
     .hero-text { font-size: 1.2rem; line-height: 1.6; color: #1c2d37; margin-bottom: 25px; }
